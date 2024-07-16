3ff851da05d1477340f7ec3347d5ff170fedccff
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-16 13:08:57 +0300

    feat(api): add user model and repository

---
+++ api/src/main/java/dev/whyneet/ec_api/frameworks/data/mongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package dev.whyneet.ec_api.frameworks.data.mongo.repository;
+
+import dev.whyneet.ec_api.core.abstracts.IDataRepository;
+import dev.whyneet.ec_api.core.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface UserRepository extends MongoRepository<User, String>, IDataRepository<User, String> {
+
+}
