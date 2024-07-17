750156e745e5f6948468b2ec5ac319d81d939923
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-17 22:52:22 +0300

    chore(api): migrate build system to gradle

--- api/src/main/java/dev/whyneet/ec_api/EcApiApplication.java
+++ api/src/main/java/dev/whyneet/ec_api/EcApiApplication.java
@@ -3,8 +3,10 @@ package dev.whyneet.ec_api;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class EcApiApplication {
 
 	public static void main(String[] args) {
