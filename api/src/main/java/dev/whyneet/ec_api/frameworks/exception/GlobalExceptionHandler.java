36f97043a282beee3f7c155f69553931d17b23fe
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-17 12:58:00 +0300

    refactor(api): specify unhandled error class in global exception handler

--- api/src/main/java/dev/whyneet/ec_api/frameworks/exception/GlobalExceptionHandler.java
+++ api/src/main/java/dev/whyneet/ec_api/frameworks/exception/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package dev.whyneet.ec_api.frameworks.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ResponseError> defaultExceptionHandler(Exception exception, HttpServletRequest req) {
-        ResponseError responseError = new ResponseError("UnhandledException", "An internal error occurred.", exception.getMessage(), req.getRequestURI());
+        ResponseError responseError = new ResponseError(String.format("UnhandledException/%s", exception.getClass().getName()), "An internal error occurred.", exception.getMessage(), req.getRequestURI());
 
         return ResponseEntity.internalServerError().body(responseError);
     }
