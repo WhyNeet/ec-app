af293261a5608ec08d1ccb143324c8817c620b5a
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-17 11:47:07 +0300

    feat(api): add expiration check method to token

--- api/src/main/java/dev/whyneet/ec_api/core/entities/Token.java
+++ api/src/main/java/dev/whyneet/ec_api/core/entities/Token.java
@@ -3,16 +3,19 @@ package dev.whyneet.ec_api.core.entities;
 import io.jsonwebtoken.Claims;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.function.Function;
 
 @Data
 public class Token {
+    private Claims claims;
     private String id;
     private String subject;
 
     public Token(Claims claims) {
-        this.id = isRefreshToken(claims) ? extractClaim(claims, c -> c.get("rti", String.class)) : extractClaim(claims, Claims::getId);
-        this.subject = extractClaim(claims, Claims::getSubject);
+        this.claims = claims;
+        this.id = isRefreshToken() ? extractClaim(c -> c.get("rti", String.class)) : extractClaim(Claims::getId);
+        this.subject = extractClaim(Claims::getSubject);
     }
 
     public Token(String id, String subject) {
@@ -20,11 +23,15 @@ public class Token {
         this.subject = subject;
     }
 
-    public <T> T extractClaim(Claims claims, Function<Claims, T> resolver) {
+    public boolean isExpired() {
+        return extractClaim(Claims::getExpiration).before(new Date());
+    }
+
+    public <T> T extractClaim(Function<Claims, T> resolver) {
         return resolver.apply(claims);
     }
 
-    public boolean isRefreshToken(Claims claims) {
+    public boolean isRefreshToken() {
         return claims.containsValue("rti");
     }
 }
