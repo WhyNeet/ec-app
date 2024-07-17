4b12fcd0e64779ccba1a13b9beffd46a0c2a4564
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-17 12:57:24 +0300

    chore(api): add throws to jwt decoder decode method

--- api/src/main/java/dev/whyneet/ec_api/core/abstracts/IJwtDecoder.java
+++ api/src/main/java/dev/whyneet/ec_api/core/abstracts/IJwtDecoder.java
@@ -1,9 +1,10 @@
 package dev.whyneet.ec_api.core.abstracts;
 
 import dev.whyneet.ec_api.core.entities.Token;
+import io.jsonwebtoken.JwtException;
 
 import java.security.Key;
 
 public interface IJwtDecoder {
-    Token decodeToken(String token, Key key);
+    Token decodeToken(String token, Key key) throws JwtException;
 }
--- api/src/main/java/dev/whyneet/ec_api/frameworks/auth/jwt/JwtDecoder.java
+++ api/src/main/java/dev/whyneet/ec_api/frameworks/auth/jwt/JwtDecoder.java
@@ -3,6 +3,7 @@ package dev.whyneet.ec_api.frameworks.auth.jwt;
 import dev.whyneet.ec_api.core.abstracts.IJwtDecoder;
 import dev.whyneet.ec_api.core.entities.Token;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 
 import javax.crypto.SecretKey;
@@ -10,7 +11,7 @@ import java.security.Key;
 
 public class JwtDecoder implements IJwtDecoder {
     @Override
-    public Token decodeToken(String token, Key key) {
+    public Token decodeToken(String token, Key key) throws JwtException {
         Claims claims = Jwts.parser().verifyWith((SecretKey) key).build().parseSignedClaims(token).getPayload();
         return new Token(claims);
     }
