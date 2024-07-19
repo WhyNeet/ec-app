8c407372d6c24cda34128dd991e8e6062d3e10bb
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-19 20:43:46 +0300

    feat(api): set token cookies user signs up

--- api/src/main/java/dev/whyneet/ec_api/controllers/AuthController.java
+++ api/src/main/java/dev/whyneet/ec_api/controllers/AuthController.java
@@ -3,9 +3,12 @@ package dev.whyneet.ec_api.controllers;
 import dev.whyneet.ec_api.core.dtos.user.CreateUserDto;
 import dev.whyneet.ec_api.core.dtos.user.UserDto;
 import dev.whyneet.ec_api.core.entities.User;
+import dev.whyneet.ec_api.features.token.TokenService;
 import dev.whyneet.ec_api.features.user.UserFactory;
 import dev.whyneet.ec_api.features.user.UserService;
+import dev.whyneet.ec_api.frameworks.auth.jwt.token.TokenPair;
 import dev.whyneet.ec_api.frameworks.exception.exceptions.UserException;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +20,15 @@ public class AuthController {
     private UserService userService;
     @Autowired
     private UserFactory userFactory;
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping("/signup")
-    public ResponseEntity<UserDto> signup(@RequestBody CreateUserDto createUserDto) throws UserException.UserAlreadyExists {
+    public ResponseEntity<UserDto> signup(@RequestBody CreateUserDto createUserDto, HttpServletResponse response) throws UserException.UserAlreadyExists {
         User user = userService.createUser(createUserDto);
+
+        TokenPair tokenPair = tokenService.generateTokenPair(user.getId(), null);
+        tokenService.setCookies(response, tokenPair);
 
         return ResponseEntity.ok(userFactory.toDto(user));
     }
