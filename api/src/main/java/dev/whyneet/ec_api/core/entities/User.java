a4aaf3ac308f05c618c2359fd131606bf0cc2d02
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-20 22:15:03 +0300

    feat(api): implement UserDetails for User entity

--- api/src/main/java/dev/whyneet/ec_api/core/entities/User.java
+++ api/src/main/java/dev/whyneet/ec_api/core/entities/User.java
@@ -5,11 +5,16 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @Document(collection = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     private String id;
     private String name;
@@ -19,4 +24,14 @@ public class User {
     private String email;
 
     private String address;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
 }
