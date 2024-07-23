2c25dc3dfcf2891fc59698e88e5769e20d089208
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-23 12:25:16 +0300

    feat(api): add seller model

---
+++ api/src/main/java/dev/whyneet/ec_api/core/entities/Seller.java
@@ -0,0 +1,20 @@
+package dev.whyneet.ec_api.core.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@Document(collection = "sellers")
+public class Seller {
+    @Id
+    private String id;
+    private String businessName;
+    @Indexed(unique = true)
+    private String taxId;
+    private boolean verified;
+    private String description;
+}
