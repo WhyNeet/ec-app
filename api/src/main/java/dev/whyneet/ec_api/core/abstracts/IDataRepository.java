d2f414f7d63202014bcd9f88ebf7a91de1979ad0
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-26 12:58:32 +0300

    feat(api): add product retrieval logic

--- api/src/main/java/dev/whyneet/ec_api/core/abstracts/IDataRepository.java
+++ api/src/main/java/dev/whyneet/ec_api/core/abstracts/IDataRepository.java
@@ -2,11 +2,13 @@ package dev.whyneet.ec_api.core.abstracts;
 
 import org.springframework.data.domain.Example;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IDataRepository<Entity, Id> {
     Optional<Entity> getById(Id id);
     <S extends Entity> Optional<S> findOne(Example<S> example);
+    <S extends Entity> List<S> findAll(Example<S> example);
     Entity save(Entity entity);
     void deleteById(Id id);
 }
--- api/src/main/java/dev/whyneet/ec_api/features/product/ProductService.java
+++ api/src/main/java/dev/whyneet/ec_api/features/product/ProductService.java
@@ -4,7 +4,11 @@ import dev.whyneet.ec_api.core.abstracts.IDataServices;
 import dev.whyneet.ec_api.core.dtos.product.CreateProductDto;
 import dev.whyneet.ec_api.core.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -17,5 +21,13 @@ public class ProductService {
         Product product = productFactory.fromCreateDto(createProductDto, sellerId);
 
         return dataServices.products().save(product);
+    }
+
+    public Optional<Product> getProductById(String productId) {
+        return dataServices.products().getById(productId);
+    }
+
+    public List<Product> getProductBySellerId(String sellerId) {
+        return dataServices.products().findAll(Example.of(new Product(null, null, null, null, null, sellerId, null)));
     }
 }
